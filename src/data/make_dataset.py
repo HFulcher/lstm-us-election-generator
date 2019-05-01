a77f7ca6b9670d515cbe5734bf8a20d67164cfc4
Huw Fulcher <fulcherhuw@gmail.com>
2019-05-01 21:52:40 +0100

    Updated requirements and changed behaviour of make_dataset.
    Removed use of click library in favour of argparse as it complies better with pylint

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
-import click
+import argparse
 import logging
-from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    logger.info(f'{input_filepath}, {output_filepath}')
 
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
 
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="Path of input file")
+    parser.add_argument("output_file", help="Path of output file")
+    args = parser.parse_args()
+
+    main(args.input_file, args.output_file)
